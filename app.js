04da3b6ef6e6ac30e69de2ddde76a1acdce157a8
anderotamm13 <anderotamm13@gmail.com>
2026-09-06 07:43:52 +0300

    Creation of REPO

---
+++ app.js
@@ -0,0 +1,6 @@
+const NOAA = {
+    kp: "https://services.swpc.noaa.gov/products/noaa-planetary-k-index.json",
+    wind: "https://services.swpc.noaa.gov/products/summary/solar-wind-speed.json",
+    mag: "https://services.swpc.noaa.gov/products/summary/solar-wind-mag-field.json",
+    aurora: "https://services.swpc.noaa.gov/json/ovation_aurora_latest.json"
+};
